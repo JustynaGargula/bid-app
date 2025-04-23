2127407230bbe913bc4e29487257fb6eeaf02c95
JustynaGargula <s221391@student.uek.krakow.pl>
2025-04-23 20:40:21 +0200

    Created database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,48 @@
 var express = require('express');
 var router = express.Router();
+const sqlite3 = require('sqlite3');
+
+const db = new sqlite3.Database('./bid_database.db', (err) => {
+  if (err) {
+    console.error('Błąd połączenia z bazą danych:', err.message);
+  } else {
+    console.log('Połączono z bazą danych SQLite.');
+  }
+});
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS tenders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    company TEXT,
+    description TEXT,
+    tender_start_time DATETIME,
+    tender_finish_time DATETIME,
+    max_budget FLOAT
+  )
+`, (err) => {
+  if (err) {
+    console.error('Błąd tworzenia tabeli "tenders":', err.message);
+  } else {
+    console.log('Tabela "tenders" została utworzona lub już istnieje.');
+  }
+});
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS bids (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tender_id INTEGER,
+    name TEXT,
+    bid_value FLOAT,
+    bid_time DATETIME
+  )
+`, (err) => {
+  if (err) {
+    console.error('Błąd tworzenia tabeli "bids":', err.message);
+  } else {
+    console.log('Tabela "bids" została utworzona lub już istnieje.');
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,6 +60,5 @@ router.get('/finished-bids-list', (req, res) => {
 router.get('/new-bid', (req, res) => {
   res.render('./pages/new-bid');
 });
-
 
 module.exports = router;
